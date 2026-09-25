9d09260ae3792c10a78b5b971316ad5b62db533c
liule <mingchenliu465@gmail.com>
2026-09-25 21:23:19 +0800

    chore: remove desktop launcher

--- create_desktop_shortcut.ps1
+++
@@ -1,9 +0,0 @@
-$root = Split-Path -Parent $MyInvocation.MyCommand.Path
-$desktop = [Environment]::GetFolderPath('Desktop')
-$shortcut = (New-Object -ComObject WScript.Shell).CreateShortcut((Join-Path $desktop 'Fund Analyzer.lnk'))
-$shortcut.TargetPath = Join-Path $root 'launch_fund_analyzer.bat'
-$shortcut.WorkingDirectory = $root
-$shortcut.Description = 'Fund Analyzer'
-$shortcut.IconLocation = "$env:SystemRoot\System32\shell32.dll,13"
-$shortcut.Save()
-Write-Host 'Desktop shortcut created.'
--- launch_fund_analyzer.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%~dp0launch_fund_analyzer.ps1"
